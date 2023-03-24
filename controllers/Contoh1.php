94f312805d2c467488712cd7fd672ff7eed6768b
safrizalfauzi <safrizalfauzi@gmail.com>
2023-03-24 14:56:21 +0700

    menambahkan file contoh1.php dan merubah settingan config/routes

---
+++ controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan<h1>";
+        echo "Nama saya Safrizal Fauzi saya tinggal di daerah Cibinong Kabupaten Bogor Provinsi Jawa Barat.";
+        echo "<h2>Terima Kasih<h2>";
+    }
+}
